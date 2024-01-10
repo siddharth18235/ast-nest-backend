428d5bbf1581316a4e4d4494f7d281f856d29c7f
siddharth18235 <malhotrasiddharth999@gmail.com>
2024-01-10 23:51:18 +0530

    webhook might fix

--- src/telegram-bot/telegram-bot.module.ts
+++ src/telegram-bot/telegram-bot.module.ts
@@ -8,20 +8,20 @@ import { User, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
     imports:[
-        TelegrafModule.forRoot({
-            token: '6559691270:AAEN2AwtYSIIkWa0_l6iYAy-r8XhqWkS3Dg',
-          }),
+        // TelegrafModule.forRoot({
+        //     token: '6559691270:AAEN2AwtYSIIkWa0_l6iYAy-r8XhqWkS3Dg',
+        //   }),
           MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-          // TelegrafModule.forRoot({
-          //   token: '6559691270:AAEN2AwtYSIIkWa0_l6iYAy-r8XhqWkS3Dg',
-          //   launchOptions: {
-          //     dropPendingUpdates: true,
-          //     webhook: {
-          //       domain: TELEGRAM_BOT_WEB_HOOKS_DOMAIN,
-          //       hookPath: TELEGRAM_BOT_WEB_HOOKS_PATH,
-          //     },
-          //   }
-          // }),
+          TelegrafModule.forRoot({
+            token: '6559691270:AAEN2AwtYSIIkWa0_l6iYAy-r8XhqWkS3Dg',
+            launchOptions: {
+              dropPendingUpdates: true,
+              webhook: {
+                domain: 'https://webhook.site',
+                hookPath: '11559d76-d04d-4635-ab6e-b5b625f759d5',
+              },
+            }
+          }),
     ],
     providers:[TelegramBotService,UserService,WeatherService]
 })
